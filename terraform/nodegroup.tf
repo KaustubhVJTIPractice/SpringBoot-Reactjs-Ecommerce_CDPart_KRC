86c2ca338dd82e04501b231c1a45d70f09cb3079
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2025-12-31 10:01:25 +0530

    Refactor EKS node group and IAM role configuration

--- terraform/nodegroup.tf
+++ terraform/nodegroup.tf
@@ -1,15 +1,46 @@
-module "node_group" {
-  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-  version = "20.8.3"
+resource "aws_eks_node_group" "ng" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "${var.cluster_name}-workers"
+  node_role_arn   = aws_iam_role.worker_role.arn
+  subnet_ids      = module.vpc.private_subnets
 
-  name         = "ecommerce-node-group"
-  cluster_name = module.eks.cluster_name
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
 
-  subnet_ids = module.vpc.private_subnets
+  instance_types = var.node_instance_types
+}
 
-  instance_types = ["t3.medium"]
+resource "aws_iam_role" "worker_role" {
+  name = "${var.cluster_name}-worker-role"
 
-  min_size     = 2
-  max_size     = 4
-  desired_size = 2
+  assume_role_policy = data.aws_iam_policy_document.worker_assume_role.json
+}
+
+data "aws_iam_policy_document" "worker_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "worker_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.worker_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_ECR_ReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.worker_role.name
 }
