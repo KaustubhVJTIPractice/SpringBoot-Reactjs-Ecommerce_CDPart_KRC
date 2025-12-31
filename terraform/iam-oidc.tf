5d4eddec067ff5b8360efa7c42e6d8a556720d4f
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2025-12-31 17:01:16 +0530

    Add OIDC provider for EKS cluster IRSA

---
+++ terraform/iam-oidc.tf
@@ -0,0 +1,17 @@
+# Fetch EKS cluster details
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.eks.name
+}
+
+# Create OIDC provider for IRSA
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "9e99a48a9960b14926bb7f3b02e22da0ecd4e9b5"
+  ]
+}
